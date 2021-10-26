4d010978ebf1cc547db1b9fc241a9fb0de4ff566
LuGuanzong <746722756@qq.com>
2021-10-26 18:50:05 +0800

    完成博客首页

--- bluelog/blueprints/blog.py
+++ bluelog/blueprints/blog.py
@@ -1,3 +1,14 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, current_app
+
+from bluelog.models import Post
 
 blog_bp = Blueprint('blog', __name__)
+
+
+@blog_bp.route('/')
+def index():
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['BLUELOG_POST_PER_PAGE']
+    pagination = Post.query.order_by(Post.timestamp.desc()).pagination(page, per_page=per_page)
+    posts = pagination.items
+    return render_template('blog/index.html', pagination=pagination, posts=posts)
