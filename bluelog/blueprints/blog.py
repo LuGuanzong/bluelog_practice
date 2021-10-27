e8e864d26914d078c45884e4f8d3d68a0da4322c
LuGuanzong <746722756@qq.com>
2021-10-27 14:40:03 +0800

    完成显示文章内容并评论功能

--- bluelog/blueprints/blog.py
+++ bluelog/blueprints/blog.py
@@ -1,8 +1,15 @@
-from flask import render_template, Blueprint, request, current_app
+from flask import render_template, Blueprint, request, current_app, url_for, flash, redirect
 
-from bluelog.models import Post
+from bluelog.emails import send_new_reply_email, send_new_comment_email
+from bluelog.models import Post, Comment
+from bluelog.forms import AdminCommentForm
+from bluelog.extensions import db
 
 blog_bp = Blueprint('blog', __name__)
+
+
+class current_user:
+    is_authenticated = False
 
 
 @blog_bp.route('/')
@@ -12,3 +19,48 @@ def index():
     pagination = Post.query.order_by(Post.timestamp.desc()).pagination(page, per_page=per_page)
     posts = pagination.items
     return render_template('blog/index.html', pagination=pagination, posts=posts)
+
+
+@blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['BLUELOG_COMMENT_PER_PAGE']
+    pagination = Comment.query.with_parent(post).filter_by(reviewed=True).order_by(Comment.timestamp.asc()).paginate(
+        page, per_page)
+    comments = pagination.items
+
+    if current_user.is_authenticated:
+        form = AdminCommentForm()
+        form.author.data = current_user.name
+        form.email.data = current_app.config['BLUELOG_EMAIL']
+        form.site.data = url_for('.index')
+        from_admin = True
+        reviewed = True
+    else:
+        form = AdminCommentForm()
+        from_admin = False
+        reviewed = False
+
+    if form.validate_on_submit():
+        author = form.author.data
+        email = form.email.data
+        site = form.site.data
+        body = form.body.data
+        comment = Comment(
+            author=author, email=email, site=site, body=body,
+            from_admin=from_admin, post=post, reviewed=reviewed)
+        replied_id = request.args.get('reply')
+        if replied_id:
+            replied_comment = Comment.query.get_or_404(replied_id)
+            comment.replied = replied_comment
+            send_new_reply_email(replied_comment)
+        db.session.add(comment)
+        db.session.commit()
+        if current_user.is_authenticated:
+            flash('Comment published.', 'success')
+        else:
+            flash('Thanks, your comment will be published after reviewed.', 'info')
+            send_new_comment_email(post)
+        return redirect(url_for('.show_post', post_id=post_id))
+    return render_template('blog/post.html', post=post, pagination=pagination, form=form, comments=comments)
